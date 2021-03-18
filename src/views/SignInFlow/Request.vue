29b874f9bb6ef97025628680a6e5c54248304835
alexsans12 <eddalexcab@gmail.com>
2021-03-17 18:50:53 -0600

    Request and recover added

--- src/views/SignInFlow/Request.vue
+++ src/views/SignInFlow/Request.vue
@@ -1,15 +1,139 @@
 <template>
-    <div>
-
+    <div
+        class="container"
+        :class="{
+            'light-background': !isDarkMode,
+            'dark-background': isDarkMode,
+        }"
+    >
+        <div class="login">
+            <img src="@/assets/DCHQ.svg" v-show="isDarkMode" alt="Logo DCHQ" />
+            <img
+                src="@/assets/DCHQ-dark.svg"
+                v-show="!isDarkMode"
+                alt="Logo DCHQ"
+            />
+            <h4 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+                Request Account
+            </h4>
+            <form @submit.prevent="onSubmit">
+                <input
+                    :class="{
+                        'light-field': isDarkMode,
+                        'dark-field': !isDarkMode,
+                    }"
+                    type="email"
+                    placeholder="Email"
+                    v-model="email"
+                    requierd
+                />
+                <button>Request Account</button>
+            </form>
+            <router-link
+                to="/signin"
+                :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
+                >Already have an account? Sign in now</router-link
+            >
+            <ThemeSwitch />
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Request'
-    }
+import ThemeSwitch from "@/components/ThemeSwitch";
+
+export default {
+    name: "Request",
+    components: {
+        ThemeSwitch,
+    },
+    data() {
+        return {
+            email: null,
+        };
+    },
+    computed: {
+        isDarkMode() {
+            return this.$store.getters.isDarkMode;
+        },
+    },
+    methods: {
+        onSubmit() {
+            const email = this.email;
+
+            // Slack API Logic
+            let slackURL = new URL("https://slack.com/api/chat.postMessage");
+
+            const data = {
+                channel: "hq-dev",
+                text: `${email} has requested admin access to HQ. Please go to Netlify to invite them.`,
+            };
+
+            const formData = new FormData();
+            formData.append('token', 'xoxb-1894260863232-1855711504215-O0ic6tytFxBbPZfkEKnHlWpF');
+
+            slackURL.search = new URLSearchParams(data);
+
+            console.log(FormData);
+
+            fetch(slackURL, { 
+                body: formData,
+                method: 'post',
+            }).then(() => {
+                this.$router.push({
+                    name: "signin",
+                    params: {
+                        userRequestedAccount: true,
+                        email
+                    },
+                });
+            }).catch(error => {
+                alert(`Error: ${error}`);
+            });
+        },
+    },
+    mounted() {
+        const params = this.$route.params;
+
+        if (params.userLoggedOut) {
+            this.hasText = true;
+            this.text = "You have logged out!";
+        }
+    },
+};
 </script>
 
 <style lang="scss" scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    text-align: center;
+}
 
+.login {
+    width: 400px;
+
+    input {
+        margin-top: 20px;
+    }
+
+    button {
+        margin-top: 20px;
+        margin-bottom: 40px;
+    }
+
+    @media only screen and (max-width: 768px) {
+        margin-top: 70px;
+
+        img {
+            width: 120px;
+        }
+
+        button {
+            margin-bottom: 30px;
+        }
+    }
+}
 </style>
--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -78,9 +78,6 @@ export default {
         },
     },
     methods: {
-        toggleDarkMode() {
-            this.$store.commit("toggleDarkMode");
-        },
         onSubmit() {
             const email = this.email;
             const password = this.password;
@@ -98,6 +95,12 @@ export default {
         if(params.userLoggedOut) {
             this.hasText = true;
             this.text = "You have logged out!";
+        } else if(params.userRecoveredAccount) {
+            this.hasText = true;
+            this.text = `A recovery email has been sent to ${params.email}` ;
+        } else if(params.userRequestedAccount) {
+            this.hasText = true;
+            this.text = `Your request has been sent to an administrator for ${params.email}`;
         }
     },
 };
